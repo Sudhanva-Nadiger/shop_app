d7943977e47dd8e93fd78da924a24461b6b01411
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-03-05 12:38:33 +0530

    style card

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,7 +12,25 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl),
+      footer: GridTileBar(
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.black54,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
