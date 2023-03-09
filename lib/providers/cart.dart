46a9b2c33d9136323bf088eda2626f0b1ff00e42
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-03-09 17:45:37 +0530

    feat: product details screen implimentation

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -14,7 +14,7 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items = {};
+  final Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -13,9 +13,53 @@ class ProductDetailScreen extends StatelessWidget {
     final loadedProduct =
         // by setting listen to false we will make sure this method will not rerun if the items list in products procider changes
         Provider.of<Products>(context, listen: false).findById(productId);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(loadedProduct.title),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image.network(
+                    loadedProduct.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: const TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: SizedBox(
+                width: double.infinity,
+                child: Text(
+                  loadedProduct.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
